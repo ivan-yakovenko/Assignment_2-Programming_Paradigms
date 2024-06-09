d9e28804f1218a4e4f1146de76e0c2e7caab0d77
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-06-09 21:29:50 +0300

    added declaration for cut and paste function added member buffer

--- Assignment_2/DynamicArray/DynamicArray.h
+++ Assignment_2/DynamicArray/DynamicArray.h
@@ -7,9 +7,10 @@ private:
     size_t rows;
     size_t cols;
     size_t capacity;
+    char *buffer;
 public:
     DynamicArray();
-    DynamicArray(char** data, size_t rows, size_t cols, size_t capacity);
+    DynamicArray(char** data, size_t rows, size_t cols, size_t capacity, char* buffer);
     ~DynamicArray();
     size_t getCapacity() const;
     void Resize(size_t newValueSize);
@@ -20,5 +21,7 @@ public:
     void SaveInfo(char* filename);
     void LoadInfo(char* filename);
     void Delete(int line, int index, int symbols);
+    void Cut(int line, int index, int symbols);
+    void Paste(int line, int index);
     void Print() const;
 };
