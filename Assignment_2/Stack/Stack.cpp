be324b14690d322e5c0460d7981c03ba161630a4
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-06-11 12:18:20 +0300

    implemented stack

--- Assignment_2/Stack/Stack.cpp
+++ Assignment_2/Stack/Stack.cpp
@@ -1,5 +1,64 @@
-//
-// Created by Ivan Yakovenko on 10.06.2024.
-//
-
 #include "Stack.h"
+
+template <typename T>
+
+Stack<T>::Stack() {
+    top = nullptr;
+    size = 0;
+};
+
+template <typename T>
+
+Stack<T>::~Stack() {
+    while (!IsEmpty()) {
+        Pop();
+    }
+}
+
+template <typename T>
+
+void Stack<T>::Push(T value) {
+    size++;
+
+    if(top == nullptr) {
+        top = new Element;
+        top->value = value;
+        return;
+    }
+
+    Element* temp = new Element;
+    temp->value = value;
+    temp->pNext = top;
+    top = temp;
+}
+
+template <typename T>
+
+T &Stack<T>::GetTop() {
+    return top->value;
+}
+
+template <typename T>
+
+bool Stack<T>::IsEmpty() const {
+    return top == nullptr;
+}
+
+template <typename T>
+
+void Stack<T>::Pop() {
+    if(top == nullptr) {
+        return;
+    }
+
+    Element* temp = top;
+    top = top->pNext;
+    delete temp;
+    size--;
+}
+
+
+
+
+
+
